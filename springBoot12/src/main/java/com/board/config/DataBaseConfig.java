9c952310d3b18d9593d9456904598693ebb68df5
GIT_PC <GIT_PC@DESKTOP-3KIP224>
2017-08-28 17:04:49 +0900

    StringBootBoard

---
+++ springBoot12/src/main/java/com/board/config/DataBaseConfig.java
@@ -0,0 +1,43 @@
+package com.board.config;
+
+import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration
+@MapperScan(basePackages = "com.board.mapper")
+@EnableTransactionManagement
+public class DataBaseConfig {
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+		sessionFactory.setMapperLocations(resolver.getResources("classpath:com/example/demo/mapper/*.xml"));
+		return sessionFactory.getObject();
+	}
+
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) throws Exception {
+		final SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory);
+		return sqlSessionTemplate;
+	}
+
+	/**
+	 * HiddenHttpMethodFilter
+	 */
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
+		return filter;
+	}
+
+}
---
+++ springBoot12/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+	pageEncoding="EUC-KR"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+
+
+	<h1>home.jsp</h1>
+    msg : ${msg}
+    
+    <form action="/member" method="get">
+    	<input type="text" name="name">
+    	<input type="text" name="age"> 
+    	<input type="submit" value="완료">
+    </form>
+    
+
+</body>
+</html>
